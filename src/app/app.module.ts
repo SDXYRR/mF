f1370506a9ec49ff80da16f27fc8c453b17c6ea7
SDXYRR <879357940@qq.com>
2018-03-25 22:55:42 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { FormListComponent } from './form-list/form-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
